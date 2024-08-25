386e7c10cc88b44e67256cbf73cb47c10af62941
Riadh Laabidi <riadh.by.laabidi@gmail.com>
2024-08-25 12:33:05 +0100

    145

---
+++ src/main/c/leetcode/0145.binary-tree-postorder-traversal/solution.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+struct TreeNode {
+  int val;
+  struct TreeNode *left;
+  struct TreeNode *right;
+};
+
+void traverse(struct TreeNode *root, int *values, int *returnSize)
+{
+    if (!root) {
+        return;
+    }
+
+    traverse(root->left, values, returnSize);
+    traverse(root->right, values, returnSize);
+
+    values[(*returnSize)++] = root->val; 
+}
+
+int* postorderTraversal(struct TreeNode *root, int *returnSize) {
+    *returnSize = 0;
+    int *values = (int*) malloc(100 * sizeof(int));
+    traverse(root, values, returnSize);
+    return values;
+}
+
