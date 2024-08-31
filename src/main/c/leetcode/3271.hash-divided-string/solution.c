d51c4c4fac62f7cde43a3a6647ee8bbf12d1d8a0
Riadh Laabidi <riadh.by.laabidi@gmail.com>
2024-08-31 20:01:51 +0100

    3271

---
+++ src/main/c/leetcode/3271.hash-divided-string/solution.c
@@ -0,0 +1,38 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* string_hash(char* s, int k) {
+    int n = strlen(s);
+    char *ans = (char*) malloc((n / k) * sizeof(char));
+
+    int c = 0;
+    int j = 0;
+
+    for (int i = 0; i < n; ++i) {
+        c += s[i] - 'a'; 
+
+	if ((i + 1) % k == 0) {
+	    ans[j++] = (char) (c % 26 + 'a');
+	    c = 0;
+	}
+    }
+
+    return ans; 
+} 
+
+int main(void) 
+{
+    char s[] = "abcd";
+    char expected[] = "bf";
+
+    char *res = string_hash(s, 2);
+
+    assert(!strcmp(res, expected));
+
+    printf("result = %s", res);
+
+    free(res);
+    return 0;
+}
