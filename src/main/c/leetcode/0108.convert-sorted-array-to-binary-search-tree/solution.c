8d81a5bdcec33d6ed1fea4e3f9537b0e079b01bb
Riadh Laabidi <riadh.by.laabidi@gmail.com>
2024-09-01 15:18:27 +0100

    0108

---
+++ src/main/c/leetcode/0108.convert-sorted-array-to-binary-search-tree/solution.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode *construct_bst(int *nums, int start, int end)
+{
+    if (start >= end) {
+        return NULL;
+    }
+
+    struct TreeNode *root = (struct TreeNode *)malloc(sizeof(struct TreeNode));
+    int middle = (start + end) / 2;
+    root->val = nums[middle];
+    root->left = construct_bst(nums, start, middle);
+    root->right = construct_bst(nums, middle + 1, end);
+
+    return root;
+}
+
+struct TreeNode *sortedArrayToBST(int *nums, int numsSize)
+{
+    return construct_bst(nums, 0, numsSize);
+}
