296dceb3278f8955f37f9d6ebe5be74407ced600
Riadh Laabidi <riadh.by.laabidi@gmail.com>
2024-09-05 11:55:53 +0100

    2028

---
+++ src/main/c/leetcode/2028.find-missing-observations/solution.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+int *missing_rolls(int *rolls, int rolls_size, int mean, int n,
+                   int *return_size)
+{
+    int missing_sum = 0;
+
+    for (int i = 0; i < rolls_size; ++i) {
+        missing_sum += rolls[i];
+    }
+
+    missing_sum = mean * (rolls_size + n) - missing_sum;
+
+    if (missing_sum > 6 * n || missing_sum < n) {
+        *return_size = 0;
+        return NULL;
+    }
+
+    int *ans = (int *)malloc(n * sizeof(int));
+    int base = missing_sum / n;
+    int remainder = missing_sum % n;
+
+    for (int i = 0; i < n; ++i) {
+        ans[i] = base + (i < remainder);
+    }
+
+    *return_size = n;
+    return ans;
+}
