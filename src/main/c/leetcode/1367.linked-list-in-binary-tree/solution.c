47a57d4a2453b1c2b85375c32b46c6ba7b03b3b4
Riadh Laabidi <riadh.by.laabidi@gmail.com>
2024-09-07 10:50:13 +0100

    1367

---
+++ src/main/c/leetcode/1367.linked-list-in-binary-tree/solution.c
@@ -0,0 +1,37 @@
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+int walk(struct ListNode *node, struct TreeNode *root)
+{
+    if (!node) {
+        return 1;
+    }
+
+    if (!root) {
+        return 0;
+    }
+
+    if (root->val == node->val) {
+        return walk(node->next, root->left) || walk(node->next, root->right);
+    }
+
+    return 0;
+}
+
+int is_sub_path(struct ListNode *head, struct TreeNode *root)
+{
+    if (!root) {
+        return 0;
+    }
+
+    return walk(head, root) || is_sub_path(head, root->left) ||
+           is_sub_path(head, root->right);
+}
