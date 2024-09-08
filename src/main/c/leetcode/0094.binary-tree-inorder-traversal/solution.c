a31472720c8f42d9db4e30c6b088b5545fa4d521
Riadh Laabidi <riadh.by.laabidi@gmail.com>
2024-09-08 12:33:47 +0100

    94

---
+++ src/main/c/leetcode/0094.binary-tree-inorder-traversal/solution.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+void walk(struct TreeNode *root, int *values, int *return_size)
+{
+    if (!root) {
+        return;
+    }
+
+    walk(root->left, values, return_size);
+    values[(*return_size)++] = root->val;
+    walk(root->right, values, return_size);
+}
+
+int *inorderTraversal(struct TreeNode *root, int *return_size)
+{
+    int *values = (int *)malloc(100 * sizeof(int));
+    *return_size = 0;
+    walk(root, values, return_size);
+
+    return values;
+}
