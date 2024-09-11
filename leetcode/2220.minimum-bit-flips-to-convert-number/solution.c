4440c66ef19a10901d06ea2385e0ee9425c7a3b1
Riadh Laabidi <riadh.by.laabidi@gmail.com>
2024-09-11 09:13:17 +0100

    2220

---
+++ leetcode/2220.minimum-bit-flips-to-convert-number/solution.c
@@ -0,0 +1,28 @@
+#include <assert.h>
+#include <stdio.h>
+
+int min_bit_flips(int start, int goal)
+{
+    int bits_to_flip = start ^ goal;
+    int ans = 0;
+
+    while (bits_to_flip) {
+        ans += bits_to_flip & 1;
+        bits_to_flip >>= 1;
+    }
+
+    return ans;
+}
+
+int main(void)
+{
+    int start = 10;
+    int goal = 7;
+
+    int expected = 3;
+    int actual = min_bit_flips(start, goal);
+
+    assert(actual == expected);
+
+    printf("Correct %d", actual);
+}
